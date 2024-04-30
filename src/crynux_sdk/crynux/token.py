ed5cdb1e3ec71cb0ea0f00c6b3ab41ed0de36997
iwehf <henry.lee@crynux.ai>
2024-04-30 12:34:13 +0800

    change deposit api param name

--- src/crynux_sdk/crynux/token.py
+++ src/crynux_sdk/crynux/token.py
@@ -13,15 +13,15 @@ class Token(object):
         self._contracts = contracts
         self._option = option
 
-    async def transfer_eth(self, address: str, eth: int):
-        if eth > 0:
+    async def transfer(self, address: str, amount: int):
+        if amount > 0:
             address = Web3.to_checksum_address(address)
             await self._contracts.transfer(
-                to=address, amount=eth, option=self._option
+                to=address, amount=amount, option=self._option
             )
-            _logger.info(f"transfer {eth} eth to {address}")
+            _logger.info(f"transfer {amount} eth to {address}")
 
-    async def eth_balance(self, address: str) -> int:
+    async def get_balance(self, address: str) -> int:
         address = Web3.to_checksum_address(address)
         token = await self._contracts.get_balance(address)
         _logger.info(f"{address} eth balance {token}")
